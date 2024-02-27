a2c81560d9aca19d3693b448ebb6ab2ea7637ae9
Sadia Khan <khansadia220@gmail.com>
2024-02-27 21:25:06 +0100

    So far search page is created

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,7 +3,9 @@ import React from "react";
 type Props = {};
 
 const SearchPage = () => {
-  return <h1>This is our search page</h1>;
+  return <h1>This is our search page
+    Let try to search something
+  </h1>;
 };
 
 export default SearchPage;
